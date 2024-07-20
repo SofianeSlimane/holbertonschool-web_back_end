ad484046e4795e0ce66b3648d601fbf66ceba7c9
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-20 15:23:28 +0200

    Removed error messages

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,40 +2,24 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
-    } else {
-      console.error('TypeError: Name must be a string');
-      console.error("    ...")
     }
-
     if (typeof length === 'number') {
       this._length = length;
-    } else {
-      console.error('TypeError: Length must be a number');
-      console.error("    ...")
     }
     if (Array.isArray(students) === true && students.every((item) => typeof item === 'string')) {
       this._students = students;
-    } else {
-      console.error('TypeError: Students must be an array of strings');
-      console.error("    ...")
     }
   }
 
   set name(newName) {
     if (typeof newName === 'string') {
       this._name = newName;
-    } else {
-      console.error('TypeError: Name must be a string');
-      console.error("    ...")
     }
   }
 
   set lenght(newLength) {
     if (typeof newLength === 'number') {
       this._length = newLength;
-    } else {
-      console.error('TypeError: Length must be a number');
-      console.error("...")
     }
   }
 
