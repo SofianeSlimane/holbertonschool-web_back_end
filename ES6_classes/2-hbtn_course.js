a0de5f3ecf307aa810dd8d9a61d5e2614de4ef8a
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-20 18:43:20 +0200

    Using private attribute at conctructor

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,17 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._name = name;
+    this._length = length;
+    if (Array.isArray(students) === true && students.every((item) => typeof item === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 
   set name(newName) {
