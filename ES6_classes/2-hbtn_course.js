597f3c9c1fa701543065bd4c2f223c4b66975b67
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-20 17:11:05 +0200

    Added error handling for students

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -22,7 +22,11 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    this._students = newStudents;
+    if (Array.isArray(newStudents) === true && newStudents.every((item) => typeof item === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 
   get name() {
