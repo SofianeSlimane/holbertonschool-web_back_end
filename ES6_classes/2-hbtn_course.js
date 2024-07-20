227e316fb15b29e6e1d208201395b884fddb48e4
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-20 16:05:59 +0200

    Using throw instead of console.error

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,30 +2,42 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
     }
     if (typeof length === 'number') {
       this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
     }
     if (Array.isArray(students) === true && students.every((item) => typeof item === 'string')) {
       this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 
   set name(newName) {
     if (typeof newName === 'string') {
       this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
   }
 
   set lenght(newLength) {
     if (typeof newLength === 'number') {
       this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
     }
   }
 
   set students(newStudents) {
     if (Array.isArray(newStudents) === true && newStudents.every((item) => typeof item === 'string')) {
       this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 
