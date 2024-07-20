2eadfe06b18a134f15cde58f8f00594cd8dd64c8
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-20 18:56:44 +0200

    Calling method within constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,18 @@
-export default  class Building {
-constructor(sqft) {
-this.sqft = sqft;
-}
-set sqft(newSqft){
-this._sqft = newSqft;
-}
-get sqft(){
-return this._sqft;
-}
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    this.evacuationWarningMessage();
+  }
 
-evacuationWarningMessage(){
-throw new Error("Class extending Building must override evacuationWarningMessage");
-}
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
