db034c81e58bdffb2ab24ae73d5cc644428d2de9
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-22 15:31:21 +0200

    Added condition if position out of range

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const myView = new DataView(buffer);
+  if (position > length || position < 0) {
+    throw new Error('Position outside range');
+  }
   myView.setInt8(position, value);
   return myView;
 }
