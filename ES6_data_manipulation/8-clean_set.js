ac7774ad264381399bd42361781600063d25ee3d
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-22 21:19:24 +0200

    Added condition

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
   let myStr = '';
   for (const elem of set) {
+    if (elem === startString) {
+      myStr.concat(`${elem}-`);
+    }
     if (elem.startsWith(startString) === true && startString.length > 0) {
       const subStr = elem.substring(startString.length, elem.length);
       myStr = myStr.concat(`${subStr}-`);
