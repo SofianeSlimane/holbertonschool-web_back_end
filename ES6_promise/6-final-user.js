2c8db8e6c2f7ab0c0d54f65534f0f8814ba4730b
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-23 23:21:14 +0200

    Returning status and value of both promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,27 +2,27 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  let obj = null;
+  const obj = [];
   try {
     await signUpUser(firstName, lastName).then((result) => {
-      obj = [{
-        status: 'fulfilled',
+      obj.push({
+        status: '<fulfilled>',
         value: result,
-      }];
+      });
     });
     await uploadPhoto(fileName).then((result) => {
-      obj = [{
-        status: 'fulfilled',
+      obj.push({
+        status: '<fulfilled>',
         value: result,
-      }];
+      });
     });
   } catch (e) {
-    obj = [
+    obj.push(
       {
-        status: 'rejected',
+        status: '<rejected>',
         value: e,
       },
-    ];
+    );
   }
   return obj;
 }
