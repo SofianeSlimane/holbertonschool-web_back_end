4f5286a2c095932c672e8d000b360511feec9c2a
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-23 23:14:42 +0200

    Using try and except statements

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,27 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  await signUpUser(firstName, lastName);
-  await uploadPhoto(fileName);
+  let obj = null;
+  try {
+    await signUpUser(firstName, lastName).then((result) => {
+      obj = [{
+        status: '<fulfilled>',
+        value: result,
+      }];
+    });
+    await uploadPhoto(fileName).then((result) => {
+      obj = [{
+        status: '<fulfilled>',
+        value: result,
+      }];
+    });
+  } catch (e) {
+    obj = [
+      {
+        status: '<rejected>',
+        value: e,
+      },
+    ];
+  }
+  return obj;
 }
