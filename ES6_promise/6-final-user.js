6f4255e9306c146b91944f9500be54b4a9276dcc
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-23 23:33:55 +0200

    Using allSettled instead of returning array manually

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,28 +1,6 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const obj = [];
-  try {
-    await signUpUser(firstName, lastName).then((result) => {
-      obj.push({
-        status: 'fulfilled',
-        value: result,
-      });
-    });
-    await uploadPhoto(fileName).then((result) => {
-      obj.push({
-        status: 'fulfilled',
-        value: result,
-      });
-    });
-  } catch (e) {
-    obj.push(
-      {
-        status: 'rejected',
-        value: e,
-      },
-    );
-  }
-  return obj;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
