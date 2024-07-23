3c8ab0d1fc4e937ff36a70e2018b527eabe5cce3
SofianeSlimane <sofianeslimane24@gmail.com>
2024-07-23 23:45:55 +0200

    Using try statement again and returning error message without traceback

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,28 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const obj = [];
+  try {
+    await signUpUser(firstName, lastName).then((result) => {
+      obj.push({
+        status: 'fulfilled',
+        value: result,
+      });
+    });
+    await uploadPhoto(fileName).then((result) => {
+      obj.push({
+        status: 'fulfilled',
+        value: result,
+      });
+    });
+  } catch (e) {
+    obj.push(
+      {
+        status: 'rejected',
+        value: e.message,
+      },
+    );
+  }
+  return obj;
 }
