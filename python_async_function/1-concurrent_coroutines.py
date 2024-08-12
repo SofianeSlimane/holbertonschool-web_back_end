eed0030033d04a35ba06c0dcb48e638a4a65cd08
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 22:58:00 +0200

    Added condition

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,11 +8,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """Executes multiple coroutines at the same time
     and returns a list of execution time in ascending order"""
-    tasks = [wait_random(max_delay) for i in range(0, n)]
-    delay_list = await asyncio.gather(*tasks)
-    asc_lst = []
-    for i in range(0, len(delay_list)):
-        asc_lst.append(min(delay_list))
-        delay_list.remove(min(delay_list))
+    if (max_delay > 0):
+        tasks = [wait_random(max_delay) for i in range(0, n)]
+        delay_list = await asyncio.gather(*tasks)
+        asc_lst = []
+        for i in range(0, len(delay_list)):
+            asc_lst.append(min(delay_list))
+            delay_list.remove(min(delay_list))
 
-    return asc_lst
+        return asc_lst
+    else:
+        return [0.0 for i in range(0, n)]
