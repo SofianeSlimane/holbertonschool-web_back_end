b70eaa65a74a16d4a1d06cfb5e6581ddbf2c8178
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 15:52:20 +0200

    Added floor function with typing

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Contains floor function"""
+import typing
+
+
+def floor(n: float) -> int:
+    """Computes floor of a float
+    Args:
+        n: a float
+    Return: floored float
+    """
+    return round(n)
