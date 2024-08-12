617d3e13c413782f0a71119c8c961e1051813f9e
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 17:55:45 +0200

    Added sum_list function with typing

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains sum_list function"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns sum of a list of float
+    Args:
+        input_list: list of float
+    Return: sum of all floats in input_list
+    """
+    return sum(input_list)
