9172db1a1469bcd48d519ab44079c5f8195ebb85
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 17:46:03 +0200

    Added to_str function with typing

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Contains to_str function"""
+import typing
+
+
+def to_str(n: float) -> str:
+    """Computes string representation of
+    a float
+    Args:
+        n: a float
+    Return: string representation of n
+    """
+    return str(n)
