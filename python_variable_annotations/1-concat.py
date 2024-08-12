51b662833bf79b4f677949bbee79604c21db629f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 15:39:33 +0200

    Added concat function with typing

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Contains concat function"""
+import typing
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings
+    Args:
+        str1: a string
+        str2: a string
+    Returns: str1 concatenated with str2
+    """
+    return str1 + str2
