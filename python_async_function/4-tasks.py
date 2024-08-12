fa3b6cd3f8bb20759a197bdc01d48a978b6d80d8
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-13 00:56:29 +0200

    Calling task_wait_random

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,14 +2,14 @@
 """Contains asynchronous coroutine task_wait_n"""
 import asyncio
 import typing
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     """Executes multiple coroutines at the same time
     and returns a list of execution time in ascending order"""
 
-    tasks = [wait_random(max_delay) for i in range(0, n)]
+    tasks = [task_wait_random(max_delay) for i in range(0, n)]
     delay_list = await asyncio.gather(*tasks)
     asc_lst = []
     for i in range(0, len(delay_list)):
