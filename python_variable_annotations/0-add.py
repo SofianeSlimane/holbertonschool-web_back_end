76b94e0a00316cdf29ff51c659d34aea0f2a61df
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 15:33:59 +0200

    Added add function with typing

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines add function """
+import typing
+
+
+def add(a: float, b: float) -> float:
+    """Sums two floats
+    Args:
+        a: float
+        b: float
+    Returns: sum as float
+    """
+    return a + b
