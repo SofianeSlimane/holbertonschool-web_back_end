de9c2f79f2ba5f879e7ca2c990aeec4c6aed5ef0
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-12 18:01:30 +0200

    Changed syntax

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Contains sum_list function"""
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """Returns sum of a list of float
     Args:
         input_list: list of float
