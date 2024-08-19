f1f60c2e316ee8e4edea29b3359bbf2c4e7a0be3
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-19 11:02:19 +0200

    Corrected type hinting

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Contains coroutine async_comprehension"""
-
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> typing.List[float]:
     """Retrieves numbers from an asynchronous generator and
     return them"""
     numbers = [number async for number in async_generator()]
