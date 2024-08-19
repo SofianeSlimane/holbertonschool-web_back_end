13e276be2434fe916e972a8ad4f8c0350e74c309
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-19 10:47:06 +0200

    Corrected type hinting

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Contais async_generator"""
 import random
 import asyncio
-from typing import AsyncIterable
+from typing import Generator
 
 
-async def async_generator() -> AsyncIterable[float]:
+async def async_generator() -> Generator[int, None, None]:
     """Returns an asynchronous generator.
     Yields values one after the other with yield, waiting
     one second before each yield"""
