ced61e88d06a4458c5d25d281d9c221140f93d82
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-19 10:16:49 +0200

    Added async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Contais async_generator"""
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[int]:
+    """Returns an asynchronous generator.
+    Yields values one after the other with yield, waiting
+    one second before each yield"""
+    for i in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
