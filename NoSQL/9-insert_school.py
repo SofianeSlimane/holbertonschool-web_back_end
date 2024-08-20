62fc8d4ce7ed1254c04f04bb647078a6eb44a894
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-21 00:22:09 +0200

    Using insert_one

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,6 +6,7 @@ def insert_school(mongo_collection, **kwargs):
     """Insert new document in collection"""
     newDict = {}
     for element, value in kwargs.items():
+        print(element, value)
         newDict[element] = value
 
-    return mongo_collection.insert(newDict)
+    return mongo_collection.insert_one(newDict)
