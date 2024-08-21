3790eaa33247dbccef07ae30ad7eb8555b59753d
SofianeSlimane <sofianeslimane24@gmail.com>
2024-08-21 02:37:05 +0200

    Not using count

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,15 +5,15 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://127.0.0.1:27017')
 nginx_collection = client.logs.nginx
 
-nginx = nginx_collection.find()
 
 get_count = nginx_collection.count_documents({"method": 'GET'})
 post_count = nginx_collection.count_documents({"method": 'POST'})
 put_count = nginx_collection.count_documents({"method": 'PUT'})
 patch_count = nginx_collection.count_documents({"method": 'PATCH'})
 delete_count = nginx_collection.count_documents({"method": 'DELETE'})
-status_count = nginx_collection.count_documents({"path": "/status"})
-print("{} logs".format(nginx.count()))
+status_count = len(list(nginx_collection.find(
+    {"method": "GET", "path": "/status"})))
+print("{} logs".format(nginx_collection.count_documents({})))
 print("Methods:")
 print(f"\tmethod GET: {get_count}")
 print(f"\tmethod POST: {post_count}")
