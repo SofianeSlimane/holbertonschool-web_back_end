37646f01107a9a264584852486ffd922ce549daa
SofianeSlimane <sofianeslimane24@gmail.com>
2024-09-16 14:56:51 +0200

    Addprogram that takes user input

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on('close', () => { console.log('This important software is now closing\n'); });
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
