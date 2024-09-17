e316b8e31e05f1ab90775acd2717132b937e1282
SofianeSlimane <sofianeslimane24@gmail.com>
2024-09-17 11:53:25 +0200

    fix: check if script is executed from terminal or piped to another process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,8 +4,10 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-rl.on('close', () => { console.log('This important software is now closing\n'); });
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
   rl.close();
 });
