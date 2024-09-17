a7a1d1f7f7fdc865ff9ab7f75b6260fe443f5fff
SofianeSlimane <sofianeslimane24@gmail.com>
2024-09-17 14:20:53 +0200

    feat: Add script that read csv file - Check if file exist - Count number of students - Count number of students per field

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('node:fs');
+
+fs.readFile('database.csv', 'utf-8', (err, data) => {
+  if (err) {
+    console.error('Cannot load the database');
+  }
+  const dataLineByLine = data.split('\n');
+
+  console.log(`Number of students: ${dataLineByLine.length - 1}`);
+
+  const fieldList = [];
+  for (let i = 1; i < dataLineByLine.length; i += 1) {
+    const line = dataLineByLine[i].split(',');
+    const field = line[3];
+    if (!fieldList.includes(field)) {
+      fieldList.push(field);
+    }
+  }
+
+  for (const fld of fieldList) {
+    let countStudentInField = 0;
+    let studentsBelongToField = '';
+    for (let i = 1; i < dataLineByLine.length; i += 1) {
+      if (dataLineByLine[i].split(',').includes(fld)) {
+        const studentsInField = dataLineByLine[i].split(',')[0];
+
+        studentsBelongToField += `${studentsInField}`;
+        if (i < dataLineByLine.length - 2) {
+          studentsBelongToField += ', ';
+        }
+        countStudentInField += 1;
+      }
+    }
+
+    console.log(`Number of students in ${fld}: ${countStudentInField}. List: ${studentsBelongToField} `);
+  }
+});
