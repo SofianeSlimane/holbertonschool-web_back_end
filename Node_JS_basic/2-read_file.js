83acad521d366e51afa1cb0cd7c14a1658bcf484
SofianeSlimane <sofianeslimane24@gmail.com>
2024-09-17 14:30:19 +0200

    fix: wrap script in function which takes path as argument

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,37 +1,40 @@
 const fs = require('node:fs');
 
-fs.readFile('database.csv', 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Cannot load the database');
-  }
-  const dataLineByLine = data.split('\n');
-
-  console.log(`Number of students: ${dataLineByLine.length - 1}`);
-
-  const fieldList = [];
-  for (let i = 1; i < dataLineByLine.length; i += 1) {
-    const line = dataLineByLine[i].split(',');
-    const field = line[3];
-    if (!fieldList.includes(field)) {
-      fieldList.push(field);
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
     }
-  }
+    const dataLineByLine = data.split('\n');
 
-  for (const fld of fieldList) {
-    let countStudentInField = 0;
-    let studentsBelongToField = '';
+    console.log(`Number of students: ${dataLineByLine.length - 1}`);
+
+    const fieldList = [];
     for (let i = 1; i < dataLineByLine.length; i += 1) {
-      if (dataLineByLine[i].split(',').includes(fld)) {
-        const studentsInField = dataLineByLine[i].split(',')[0];
-
-        studentsBelongToField += `${studentsInField}`;
-        if (i < dataLineByLine.length - 2) {
-          studentsBelongToField += ', ';
-        }
-        countStudentInField += 1;
+      const line = dataLineByLine[i].split(',');
+      const field = line[3];
+      if (!fieldList.includes(field)) {
+        fieldList.push(field);
       }
     }
 
-    console.log(`Number of students in ${fld}: ${countStudentInField}. List: ${studentsBelongToField} `);
-  }
-});
+    for (const fld of fieldList) {
+      let countStudentInField = 0;
+      let studentsBelongToField = '';
+      for (let i = 1; i < dataLineByLine.length; i += 1) {
+        if (dataLineByLine[i].split(',').includes(fld)) {
+          const studentsInField = dataLineByLine[i].split(',')[0];
+
+          studentsBelongToField += `${studentsInField}`;
+          if (i < dataLineByLine.length - 2) {
+            studentsBelongToField += ', ';
+          }
+          countStudentInField += 1;
+        }
+      }
+
+      console.log(`Number of students in ${fld}: ${countStudentInField}. List: ${studentsBelongToField} `);
+    }
+  });
+}
+module.exports = countStudents;
